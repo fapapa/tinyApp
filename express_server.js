fe25d08b4517d4a3c95ae584f7f94597bea92289
Fabio Papa <fabtheman@gmail.com>
2019-09-30 15:47:23 -0700

    Add endpoint to delete a url

--- express_server.js
+++ express_server.js
@@ -66,6 +66,11 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
