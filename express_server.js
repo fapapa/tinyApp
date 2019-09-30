1f41777090771f48a17ca5a5b637d61ab8350f42
Fabio Papa <fabtheman@gmail.com>
2019-09-30 14:59:26 -0700

    Have /u/<shortURL> perform a redirect to the long URL

--- express_server.js
+++ express_server.js
@@ -34,6 +34,10 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.get('/u/:shortURL', (req, res) => {
+  res.redirect(urlDatabase[req.params.shortURL]);
+});
+
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
