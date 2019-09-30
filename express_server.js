3be13962d623afff6146cf272cadd013d2dc78cf
Fabio Papa <fabtheman@gmail.com>
2019-09-30 13:38:33 -0700

    Add route/view for /urls/<shortURL> to display shortURL details

--- express_server.js
+++ express_server.js
@@ -22,6 +22,15 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+
+  res.render('urls_show', templateVars);
+});
+
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
